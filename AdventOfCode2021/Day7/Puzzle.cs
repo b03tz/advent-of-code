a3d046de42747d80a0bf318b4f826ace81584730
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-07 21:06:15 +0100

    Refactored part 1

--- AdventOfCode2021/Day7/Puzzle.cs
+++ AdventOfCode2021/Day7/Puzzle.cs
@@ -23,24 +23,11 @@ namespace AdventOfCode2021.Day7
             var heighest = CrabPositions.Max();
             var lowest = CrabPositions.Min();
 
-            // Find the cheapest goto position
-            int leastCost = Int32.MaxValue;
-            int gotoPos = 0;
-            for (var i = lowest; i <= heighest; i++)
-            {
-                var cost = CrabPositions
-                    .Where(x => x != i)
-                    .Select(x => Math.Abs(i - x))
-                    .Sum();
-
-                if (cost < leastCost)
-                {
-                    gotoPos = i;
-                    leastCost = cost;
-                }
-            }
+            // First calculate the cheapest starting position by increments of 50 and then the actual accurate position
+            var (startingValue, uselessCost) = CalculateCheapestPosition(50, lowest, heighest);
+            var (cheapestPosition, cost) = CalculateCheapestPosition(1, startingValue - 50, heighest);
             
-            Console.WriteLine($"The cheapest position = {gotoPos} for {leastCost} fuel");
+            Console.WriteLine($"The cheapest position = {cheapestPosition} for {cost} fuel");
         }
         
         public void Part2()
@@ -48,16 +35,14 @@ namespace AdventOfCode2021.Day7
             var heighest = CrabPositions.Max();
             var lowest = CrabPositions.Min();
             
-            // First calculate the cheapest starting position by increments of 50
-            var (startingValue, uselessCost) = CalculateCheapestPosition(50, lowest, heighest);
-            
-            // Now calculate an accurate position
-            var (cheapestPosition, cost) = CalculateCheapestPosition(1, startingValue - 50, heighest);
+            // First calculate the cheapest starting position by increments of 50 and then the actual accurate position
+            var (startingValue, uselessCost) = CalculateCheapestPosition(50, lowest, heighest, true);
+            var (cheapestPosition, cost) = CalculateCheapestPosition(1, startingValue - 50, heighest, true);
             
             Console.WriteLine($"The cheapest position = {cheapestPosition} for {cost} fuel");
         }
 
-        private (int, int) CalculateCheapestPosition(int increment, int lowest, int heighest)
+        private (int, int) CalculateCheapestPosition(int increment, int lowest, int heighest, bool fuelCostIncreases = false)
         {
             // First calculate every 50th step to see where it starts getting more expensive
             var oldCost = Int32.MaxValue;
@@ -66,7 +51,7 @@ namespace AdventOfCode2021.Day7
             {
                 var cost = CrabPositions
                     .Where(x => x != s)
-                    .Select(x => CalculateCost(Math.Abs(s - x)))
+                    .Select(x => CalculateCost(Math.Abs(s - x), fuelCostIncreases))
                     .Sum();
 
                 if (cost < oldCost)
@@ -83,8 +68,11 @@ namespace AdventOfCode2021.Day7
             return (startingValue, oldCost);
         }
 
-        private int CalculateCost(int travelDistance)
+        private static int CalculateCost(int travelDistance, bool fuelCostIncreases)
         {
+            if (!fuelCostIncreases)
+                return travelDistance;
+            
             var cost = 0;
             for (var i = 1; i <= travelDistance; i++)
                 cost += i;
