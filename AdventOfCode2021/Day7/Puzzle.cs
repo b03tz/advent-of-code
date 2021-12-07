7c19cfef81e2f33d0cf45e5e6bf6e3baa7d9a46a
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-07 21:02:12 +0100

    Quicker solution :)

--- AdventOfCode2021/Day7/Puzzle.cs
+++ AdventOfCode2021/Day7/Puzzle.cs
@@ -47,25 +47,40 @@ namespace AdventOfCode2021.Day7
         {
             var heighest = CrabPositions.Max();
             var lowest = CrabPositions.Min();
+            
+            // First calculate the cheapest starting position by increments of 50
+            var (startingValue, uselessCost) = CalculateCheapestPosition(50, lowest, heighest);
+            
+            // Now calculate an accurate position
+            var (cheapestPosition, cost) = CalculateCheapestPosition(1, startingValue - 50, heighest);
+            
+            Console.WriteLine($"The cheapest position = {cheapestPosition} for {cost} fuel");
+        }
 
-            // Find the cheapest goto position
-            int leastCost = Int32.MaxValue;
-            int gotoPos = 0;
-            for (var i = lowest; i <= heighest; i++)
+        private (int, int) CalculateCheapestPosition(int increment, int lowest, int heighest)
+        {
+            // First calculate every 50th step to see where it starts getting more expensive
+            var oldCost = Int32.MaxValue;
+            var startingValue = 0;
+            for (var s = lowest; s <= (heighest + increment); s += increment)
             {
                 var cost = CrabPositions
-                    .Where(x => x != i)
-                    .Select(x => CalculateCost(Math.Abs(i - x)))
+                    .Where(x => x != s)
+                    .Select(x => CalculateCost(Math.Abs(s - x)))
                     .Sum();
 
-                if (cost < leastCost)
+                if (cost < oldCost)
                 {
-                    gotoPos = i;
-                    leastCost = cost;
+                    oldCost = cost;
+                    startingValue = s;
+                    continue;
                 }
+                
+                // It's getting more expensive so break
+                break;
             }
             
-            Console.WriteLine($"The cheapest position = {gotoPos} for {leastCost} fuel");
+            return (startingValue, oldCost);
         }
 
         private int CalculateCost(int travelDistance)
