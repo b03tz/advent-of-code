7cecd3b190f42f0079ed539bef9baf76f8396e78
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-07 09:27:40 +0100

    Day 7

---
+++ AdventOfCode2021/Day7/Puzzle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Day7
+{
+    public class Puzzle
+    {
+        private int[] ShipPositions;
+        
+        public Puzzle()
+        {
+            var content = File.ReadAllText("Day7\\input.txt");
+            
+            ShipPositions = content.Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Part1();
+            Part2();
+        }
+
+        public void Part1()
+        {
+            var heighest = ShipPositions.Max();
+            var lowest = ShipPositions.Min();
+
+            // Find the cheapest goto position
+            int leastCost = Int32.MaxValue;
+            int gotoPos = 0;
+            for (var i = lowest; i <= heighest; i++)
+            {
+                var cost = ShipPositions
+                    .Where(x => x != i)
+                    .Select(x => Math.Abs(i - x))
+                    .Sum();
+
+                if (cost < leastCost)
+                {
+                    gotoPos = i;
+                    leastCost = cost;
+                }
+            }
+            
+            Console.WriteLine($"The cheapest position = {gotoPos} for {leastCost} fuel");
+        }
+        
+        public void Part2()
+        {
+            var heighest = ShipPositions.Max();
+            var lowest = ShipPositions.Min();
+
+            // Find the cheapest goto position
+            int leastCost = Int32.MaxValue;
+            int gotoPos = 0;
+            for (var i = lowest; i <= heighest; i++)
+            {
+                var cost = ShipPositions
+                    .Where(x => x != i)
+                    .Select(x => CalculateCost(Math.Abs(i - x)))
+                    .Sum();
+
+                if (cost < leastCost)
+                {
+                    gotoPos = i;
+                    leastCost = cost;
+                }
+            }
+            
+            Console.WriteLine($"The cheapest position = {gotoPos} for {leastCost} fuel");
+        }
+
+        private int CalculateCost(int travelDistance)
+        {
+            var cost = 0;
+            for (var i = 1; i <= travelDistance; i++)
+                cost += i;
+
+            return cost;
+        }
+    }
+}
