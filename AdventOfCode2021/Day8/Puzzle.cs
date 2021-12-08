fc80cdbab3feebb3abe3789f31f009060b984b1b
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-08 12:20:51 +0100

    Day 8 - worst possible solution

--- AdventOfCode2021/Day8/Puzzle.cs
+++ AdventOfCode2021/Day8/Puzzle.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Microsoft.VisualBasic.FileIO;
 
 namespace AdventOfCode2021.Day8
 {
     public class Puzzle
     {
-        private readonly Dictionary<int, int> DigitCount = new Dictionary<int, int>
+        private readonly List<string> Permutations = new List<string>();
+
+        private readonly List<string> Numbers = new List<string>
         {
-            {0, 6},
-            {1, 2},
-            {2, 5},
-            {3, 5},
-            {4, 4},
-            {5, 5},
-            {6, 6},
-            {7, 3},
-            {8, 7},
-            {9, 6},
+            "1110111",
+            "0010010",
+            "1011101",
+            "1011011",
+            "0111010",
+            "1101011",
+            "1101111",
+            "1010010",
+            "1111111",
+            "1111011",
         };
         
         private List<string> SignalPatterns = new List<string>();
@@ -28,7 +31,8 @@ namespace AdventOfCode2021.Day8
         
         public Puzzle()
         {
-            var content = File.ReadAllText("Day8\\testinput.txt");
+            var content = File.ReadAllText("Day8\\input.txt");
+            Permutations = File.ReadAllText("Day8\\permutations.txt").Split("\n").Select(x => x.Trim()).ToList();
 
             var lines = content.Split("\n");
 
@@ -67,22 +71,96 @@ namespace AdventOfCode2021.Day8
 
         private void Part2()
         {
-            var segment = "0123456";
-            var input = "1011011";
+            var standardDigits = Numbers.Select(x => CreateDigit("0123456", x).Cast<bool>()).ToList();
+            List<string> outputNumbers = new List<string>();
+            for (var i = 0; i < SignalPatterns.Count; i++)
+            {
+                var signal = SignalPatterns[i];
+                var output = OutputValues[i];
+
+                // First convert to dictionary
+                var letters = signal.Split(' ');
+
+                // Try to work out board logic based on known numbers
+                var lastPermutation = "";
+                var lastNumberSegment = "";
+                foreach (var possiblePermutation in Permutations)
+                {
+                    var numberSegment = SegmentToNumbers(possiblePermutation);
+                    var foundPermutation = true;
+                    var numberOfMatches = 0;
+
+                    foreach (var letter in letters)
+                    {
+                        var letterBinary = ToBinary(possiblePermutation, letter);
+                        var createdDigit = CreateDigit(numberSegment, letterBinary).Cast<bool>();
+
+                        if (!standardDigits.Any(x => x.SequenceEqual(createdDigit)))
+                        {
+                            foundPermutation = false;
+                        }
+                        else
+                        {
+                            numberOfMatches += 1;
+                        }
+                    }
+
+                    if (foundPermutation)
+                    {
+                        lastPermutation = possiblePermutation;
+                        lastNumberSegment = numberSegment;
+
+                        break;
+                    }
+                }
+
+                var outputLetters = output.Split(' ');
+                var stringOutput = "";
+                foreach (var letter in outputLetters)
+                {
+                    var letterBinary = ToBinary(lastPermutation, letter);
+                    var createdDigit = CreateDigit(lastNumberSegment, letterBinary).Cast<bool>();
+                    var digit = standardDigits.FindIndex(x => x.SequenceEqual(createdDigit));
+
+                    stringOutput += digit.ToString();
+                }
+                
+                outputNumbers.Add(stringOutput);
+                Console.WriteLine(stringOutput);
+            }
             
-            PrintNumber(segment, input);
+            Console.WriteLine($"Total added: {outputNumbers.Select(x => Convert.ToInt32(x)).Sum()}");
         }
 
-        private void PrintNumber(string segment, string input)
+        private string SegmentToNumbers(string segment)
+        {
+            return segment.Replace("a", "0").Replace("b", "1").Replace("c", "2").Replace("d", "3").Replace("e", "4").Replace("f", "5").Replace("g", "6");
+        }
+
+        private string ToBinary(string letterSegment, string letter)
+        {
+            var output = new StringBuilder("0000000");
+            for (var i = 0; i < letterSegment.Length; i++)
+            {
+                if (letter.Contains(letterSegment[i]))
+                {
+                    output[letterSegment.IndexOf(letterSegment[i])] = '1';
+                }
+            }
+
+            return output.ToString();
+        }
+
+        private bool[,] CreateDigit(string numericSegment, string input)
         {
             var output = new bool[7, 7];
 
-            for (var i = 0; i < segment.Length; i++)
+            for (var i = 0; i < numericSegment.Length; i++)
             {
                 if (Convert.ToInt32(input.Substring(i, 1)) == 0)
                     continue;
                 
-                switch (Convert.ToInt32(segment.Substring(i, 1)))
+                switch (i)
                 {
                     case 0:
                         output[0, 1] = true;
@@ -123,6 +201,13 @@ namespace AdventOfCode2021.Day8
                         break;
                 }
             }
+
+            return output;
+        }
+
+        private void PrintNumber(string segment, string input)
+        {
+            var output = CreateDigit(segment, input);
 
             var outputString = "";
             for (int i = 0; i < output.GetLength(0); i++)
