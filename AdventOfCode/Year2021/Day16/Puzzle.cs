f1c08def5cc70becf177db060dfb8adcae68651e
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-16 12:11:48 +0100

    Day 16

--- AdventOfCode/Year2021/Day16/Puzzle.cs
+++ AdventOfCode/Year2021/Day16/Puzzle.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Numerics;
 
 namespace AdventOfCode.Year2021.Day16
 {
@@ -15,11 +13,58 @@ namespace AdventOfCode.Year2021.Day16
             var line = this.GetPuzzleText();
             
             var packetData = line.HexToBinary();
-            //ParsePackets("110100101111111000101000");
-            //ParsePackets("11010010111111100010100000111000000000000110111101000101001010010001001000000000");
-            //ParsePackets(packetData);
-            ParsePackets("620080001611562C8802118E34".HexToBinary());
-            //ParsePackets("11101110000000001101010000001100100000100011000001100000");
+            ParsePackets(packetData);
+            
+            Part1();
+            Part2();
+        }
+        
+        private void Part1()
+        {
+            long versionBase = 0;
+            versionBase += Packets.Select(GetVersionCount).Sum();
+            
+            Console.WriteLine($"Total versions: {versionBase}");
+        }
+
+        private void Part2()
+        {
+            long total = CalculatePacket(Packets.First());
+            
+            Console.WriteLine($"Total outer packet sum: {total}");
+        }
+
+        private long CalculatePacket(Packet packet)
+        {
+            switch (packet.TypeId)
+            {
+                case 0:
+                    // Sum
+                    return packet.SubPackets.Aggregate(0L, (sum, subPacket) => sum + CalculatePacket(subPacket));
+                case 1:
+                    // Product
+                    return packet.SubPackets.Aggregate(1L, (product, subPacket) => product * CalculatePacket(subPacket));                    
+                case 2:
+                    // Minimum
+                    return packet.SubPackets.Aggregate(Int64.MaxValue, (min, subPacket) => Math.Min(min, CalculatePacket(subPacket)));
+                case 3:
+                    // Maximum
+                    return packet.SubPackets.Aggregate(Int64.MinValue, (max, subPacket) => Math.Max(max, CalculatePacket(subPacket)));
+                case 4:
+                    // Literal
+                    return packet.LiteralValues.Sum();
+                case 5:
+                    // Greater than
+                    return CalculatePacket(packet.SubPackets.First()) > CalculatePacket(packet.SubPackets.Last()) ? 1L : 0L;
+                case 6:
+                    // Less than
+                    return CalculatePacket(packet.SubPackets.First()) < CalculatePacket(packet.SubPackets.Last()) ? 1L : 0L;
+                case 7:
+                    // Equal to
+                    return CalculatePacket(packet.SubPackets.First()) == CalculatePacket(packet.SubPackets.Last()) ? 1L : 0L;
+            }
+
+            return int.MaxValue;
         }
 
         private void ParsePackets(string data)
@@ -30,7 +75,16 @@ namespace AdventOfCode.Year2021.Day16
             }
         }
 
-        public string ParseFirstPacketAndReturnRest(string data, int versionSum = 0, Packet parentPacket = null)
+        public long GetVersionCount(Packet parent = null)
+        {
+            var version = parent.Version;
+
+            version += parent.SubPackets.Select(x => GetVersionCount(x)).Sum();
+
+            return version;
+        }
+
+        public string ParseFirstPacketAndReturnRest(string data, int versionSum = 0, Packet? parentPacket = null)
         {
             if (data.Length < 6)
                 return "";
@@ -39,7 +93,10 @@ namespace AdventOfCode.Year2021.Day16
             var type = data.Substring(3, 3).BinaryToDec();
             data = data.Substring(6);
 
-            versionSum += version;
+            if (data.IsAllZeros())
+                return "";
+
+            versionSum += Convert.ToInt32(version);
         
             switch (type)
             {
@@ -54,71 +111,85 @@ namespace AdventOfCode.Year2021.Day16
                     packetData += data.Substring(1, 4);
                     data = data.Substring(5);
 
-                    Packets.Add(new Packet
+                    if (parentPacket != null)
                     {
-                        Version = version,
-                        VersionSum = versionSum,
-                        TypeId = type,
-                        LiteralValues = new List<int> { packetData.BinaryToDec() }
-                    });
-                    
-                    // Keep removing 0's
-                    /*while (data.Length > 0 && data.Substring(0, 1) == "0")
-                        data = data.Substring(1);*/
+                        parentPacket.SubPackets.Add(new Packet
+                        {
+                            Version = version,
+                            VersionSum = versionSum,
+                            TypeId = type,
+                            LiteralValues = new List<long> { packetData.BinaryToDec() }
+                        });
+                    }
+                    else
+                    {
+                        Packets.Add(new Packet
+                        {
+                            Version = version,
+                            VersionSum = versionSum,
+                            TypeId = type,
+                            LiteralValues = new List<long> { packetData.BinaryToDec() }
+                        });
+                    }
 
                     return data;
                 default:
+                    if (data.Length < 1)
+                        return "";
+                    
                     var lengthTypeId = data.Substring(0, 1);
                     data = data.Substring(1);
-
+                    
                     switch (lengthTypeId)
                     {
                         case "0":
+                            if (data.Length < 15)
+                                return "";
+                            
                             var subPacketLength = data.Substring(0, 15).BinaryToDec();
                             data = data.Substring(15);
 
-                            var subPacketString = data.Substring(0, subPacketLength);
-                            data = data.Substring(subPacketLength);
+                            var subPacketString = data.Substring(0, Convert.ToInt32(subPacketLength));
+                            data = data.Substring(Convert.ToInt32(subPacketLength));
 
-                            var subPackets = new List<string>();
                             
-                            // Unpack subpackets
-                            while (subPacketString.Length > 5)
+                            var newPacket = new Packet()
                             {
-                                var subPacketHeader = subPacketString.Substring(0, 6);
-                                var subPacketData = subPacketString.Substring(6);
-                                subPacketString = subPacketString.Substring(6);
-                                
-                                // Keep reading subpacket data in blocks of 5 until 0
-                                var readSubPacket = "";
-                                while (subPacketData.Substring(0, 1) != "0")
-                                {
-                                    readSubPacket += subPacketData.Substring(0, 5);
-                                    subPacketData = subPacketData.Substring(5);
-                                    subPacketString = subPacketString.Substring(5);
-                                }
-                                
-                                readSubPacket += subPacketData.Substring(0, 5);
-                                subPacketData = subPacketData.Substring(5);
-                                subPacketString = subPacketString.Substring(5);
-                                
-                                subPackets.Add(subPacketHeader + readSubPacket);
-                            }
+                                Version = version,
+                                TypeId = type,
+                                SubPackets = new List<Packet>()
+                            };
+                            
+                            if (parentPacket == null)
+                                Packets.Add(newPacket);
+                            else
+                                parentPacket.SubPackets.Add(newPacket);
 
-                            foreach (var subPacket in subPackets)
-                            {
-                                ParseFirstPacketAndReturnRest(subPacket, versionSum);
-                            }
-
+                            while (!subPacketString.IsAllZeros())
+                                subPacketString = ParseFirstPacketAndReturnRest(subPacketString, versionSum, newPacket);    
+                            
                             return data;
                         case "1":
+                            if (data.Length < 11)
+                                return "";
+                            
                             var numberOfSubPackets = data.Substring(0, 11).BinaryToDec();
                             data = data.Substring(11);
+                            
+                            var newParentPacket = new Packet()
+                            {
+                                Version = version,
+                                TypeId = type,
+                                SubPackets = new List<Packet>()
+                            };
+                            
+                            if (parentPacket == null)
+                                Packets.Add(newParentPacket);
+                            else
+                                parentPacket.SubPackets.Add(newParentPacket);
 
                             for (var i = 0; i < numberOfSubPackets; i++)
-                            {
-                                data = ParseFirstPacketAndReturnRest(data, versionSum);
-                            }
+                                data = ParseFirstPacketAndReturnRest(data, versionSum, newParentPacket);
 
                             return data;
 
@@ -134,18 +205,22 @@ namespace AdventOfCode.Year2021.Day16
     
     public class Packet
     {
-        public int Version { get; set; }
-        public int VersionSum { get; set; }
-        public int TypeId { get; set; }
-        public List<int> LiteralValues { get; set; } = new List<int>();
-        public List<Packet> SubPackets { get; set; }
+        public long Version { get; set; }
+        public long VersionSum { get; set; }
+        public long TypeId { get; set; }
+        public List<long> LiteralValues { get; set; } = new List<long>();
+        public List<Packet> SubPackets { get; set; } = new List<Packet>();
     }
 
     public static class Helpers
     {
-        public static int BinaryToDec(this string binary)
+        public static bool IsAllZeros(this string tester)
         {
-            return Convert.ToInt32(binary, 2);
+            return tester.IndexOf("1", StringComparison.Ordinal) == -1 || tester.Length == 0;
+        }
+        public static long BinaryToDec(this string binary)
+        {
+            return Convert.ToInt64(binary, 2);
         }
 
         public static string HexToBinary(this string hexValue)
