95bdd93fc25156fb6df4365ff40135975f3a3efc
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-17 09:01:53 +0100

    Day 17 - bruteforced

--- AdventOfCode/Year2021/Day17/Puzzle.cs
+++ AdventOfCode/Year2021/Day17/Puzzle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Intrinsics.X86;
 using System.Text.RegularExpressions;
 
@@ -8,7 +9,7 @@ namespace AdventOfCode.Year2021.Day17
     {
         public Puzzle()
         {
-            this.Init(17, true);
+            this.Init(17, false);
             var input = this.GetPuzzleText().Trim();
 
             var match = new Regex("(x=([\\-0-9]{1,})\\.\\.([\\-0-9]{1,})), (y=([\\-0-9]{1,})\\.\\.([\\-0-9]{1,}))").Match(input);
@@ -19,35 +20,98 @@ namespace AdventOfCode.Year2021.Day17
             var yE = Convert.ToInt32(match.Groups[5].Value);
             
             Part1((xS, xE, yS, yE));
+            Part2((xS, xE, yS, yE));
         }
 
         public void Part1((int xS, int xE, int yS, int yE) targetArea)
         {
-            var currentPosition = (0, 0);
-            var velocity = (0, 0);
-
-            var test1 = DoesEnterTarget((6, 2), targetArea);
-            var test2 = DoesEnterTarget((6, 3), targetArea);
-            var test3 = DoesEnterTarget((9, 0), targetArea);
-            var test4 = DoesEnterTarget((17, -4), targetArea);
+            var currentX = MinimumXVelocity(targetArea.xS);
+            var currentY = 1;
+            var maxY = 0;
+            var bestVelocity = (currentX, currentY);
             
-            // Determine horizontal velocity
-            if (targetArea.xS > 0 && targetArea.xE > 0)
+            while (true)
             {
-                // Shooting to the right
-            }
-            else
-            {
-                // Shooting to the left
-            }
+                var yTries = 0;
+                while (true)
+                {
+                    var testVelocity = (currentX, currentY);
+                    // Find heighest y for current x
+                    if (DoesEnterTarget(testVelocity, targetArea))
+                    {
+                        var maxYVelocity = CalculateMaximumY(testVelocity);
+                        if (maxYVelocity > maxY)
+                        {
+                            bestVelocity = testVelocity;
+                            maxY = maxYVelocity;
+                        }
+                    }
 
-            if (targetArea.yS < 0 && targetArea.yE < 0)
-            {
-                // Shooting below
+                    yTries++;
+                    currentY++;
+
+                    if (yTries > 250)
+                        break;
+                }
+
+                currentY = 1;
+                currentX++;
+
+                if (currentX >= targetArea.xE)
+                    break;
             }
-            else
+            
+            Console.WriteLine($"Part 1 - The best shooting position is {bestVelocity.currentX}, {bestVelocity.currentY} with a maximum Y of {CalculateMaximumY(bestVelocity)}");
+        }
+        
+        private void Part2((int xS, int xE, int yS, int yE) targetArea)
+        {
+            var velocities = new HashSet<(int vX, int vY)>();
+
+            // Positive Y
+            LoopVelocityRange(velocities, targetArea, "up");
+            LoopVelocityRange(velocities, targetArea, "down");
+            
+            Console.WriteLine($"Part 2 - Total velocities reaching goal: {velocities.Count}");
+        }
+
+        private void LoopVelocityRange(
+            HashSet<(int, int)> velocities, 
+            (int xS, int xE, int yS, int yE) targetArea,
+            string direction)
+        {
+            var currentX = MinimumXVelocity(targetArea.xS) - 3;
+            var maxX = targetArea.xE;
+            
+            var currentY = 1;
+            while (true)
             {
-                // Shooting up
+                var yTries = 0;
+                while (true)
+                {
+                    var testVelocity = (currentX, currentY);
+                    // Find heighest y for current x
+                    if (DoesEnterTarget(testVelocity, targetArea))
+                    {
+                        velocities.Add(testVelocity);
+                    }
+
+                    yTries++;
+
+                    if (direction == "up")
+                        currentY++;
+                    else
+                        currentY--;
+
+                    if (yTries > 250)
+                        break;
+                }
+
+                if (currentX > maxX)
+                    break;
+                
+                currentY = 0;
+                currentX++;
             }
         }
 
@@ -66,8 +130,45 @@ namespace AdventOfCode.Year2021.Day17
                 if (HasOvershotTargetArea(position, targetArea))
                     return false;
 
-                velocity.vX -= 1;
+                if (velocity.vX > 0)
+                    velocity.vX -= 1;
                 velocity.vY -= 1;
+            }
+        }
+        
+        private int CalculateMaximumY((int vX, int vY) velocity)
+        {
+            (int x, int y) position = (0, 0);
+            
+            while (true)
+            {
+                position.x += velocity.vX;
+                position.y += velocity.vY;
+
+                if (velocity.vY == 0)
+                    return position.y;
+
+                if (velocity.vX > 0)
+                    velocity.vX -= 1;
+                velocity.vY -= 1;
+            }
+        }
+
+        private int MinimumXVelocity(int targetX)
+        {
+            var minimumX = 2;
+            while (true)
+            {
+                var result = 0;
+                for (var i = minimumX; i > 0; i--)
+                {
+                    result += i;
+                }
+
+                if (result >= targetX)
+                    return minimumX;
+
+                minimumX += 1;
             }
         }
 
