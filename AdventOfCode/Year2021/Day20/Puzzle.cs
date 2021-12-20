c4b719ec97fc143bff60985dd5eb75e83a2d8d67
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-20 14:38:11 +0100

    Day 20 - refacotr

--- AdventOfCode/Year2021/Day20/Puzzle.cs
+++ AdventOfCode/Year2021/Day20/Puzzle.cs
@@ -43,12 +43,13 @@ namespace AdventOfCode.Year2021.Day20
         private string[,] Enhance(string[,] image, int numberOfTimes)
         {
             Console.WriteLine($"Enhancing {numberOfTimes} times...");
+            
             var unknownUniverse = ".";
             for (var iteration = 0; iteration < numberOfTimes; iteration++)
             {
-                Console.Write(iteration + 1+ "...");
-                var paddingSize = 1;
-                image = image.PadArray(unknownUniverse, paddingSize);
+                Console.Write(iteration + 1 + "...");
+
+                image = image.PadArray(unknownUniverse, 1);
                 var outputImage = new string[image.GetLength(0), image.GetLength(1)];
             
                 // Fill output image with unknownUniverse
@@ -57,19 +58,12 @@ namespace AdventOfCode.Year2021.Day20
                         outputImage[row, col] = unknownUniverse;
                     
                 for (var row = 0; row < outputImage.GetLength(0); row++)
-                {
                     for (var col = 0; col < outputImage.GetLength(1); col++)
-                    {
-                        outputImage[row, col] = GetPixel(image, row, col, unknownUniverse);
-                    }
-                }
+                        outputImage[row, col] = ConvertPixel(image, row, col, unknownUniverse);
 
                 image = outputImage;
 
-                if (unknownUniverse == ".")
-                    unknownUniverse = Algorhithm.Substring(0, 1);
-                else
-                    unknownUniverse = ".";
+                unknownUniverse = unknownUniverse == "." ? Algorhithm.Substring(0, 1) : ".";
             }
             
             Console.Write("\n");
@@ -77,55 +71,20 @@ namespace AdventOfCode.Year2021.Day20
             return image;
         }
 
-        private (int xS, int xE, int yS, int yE) GetImageBounds(string[,] image)
-        {
-            var xS = Int32.MaxValue;
-            var xE = 0;
-            var yS = Int32.MaxValue;
-            var yE = 0;
-            
-            for (var row = 0; row < image.GetLength(0); row++)
-                for (var col = 0; col < image.GetLength(1); col++)
-                {
-                    if (image[row, col] == "#")
-                    {
-                        if (col < xS)
-                            xS = col;
-
-                        if (col > xE)
-                            xE = col;
-
-                        if (row < yS)
-                            yS = row;
-
-                        if (row > yS)
-                            yE = row;
-                    }
-                }
-
-            return (xS, xE, yS, yE);
-        }
-
-        private string GetPixel(string[,] outputImage, int row, int col, string unknownUniverse)
+        private string ConvertPixel(string[,] outputImage, int row, int col, string unknownUniverse)
         {
             var pixelString =
-                outputImage.GetFromArray(row - 1, col - 1, unknownUniverse) +
-                outputImage.GetFromArray(row - 1, col, unknownUniverse) +
-                outputImage.GetFromArray(row - 1, col + 1, unknownUniverse) +
-                outputImage.GetFromArray(row, col - 1, unknownUniverse) +
-                outputImage.GetFromArray(row, col, unknownUniverse) +
-                outputImage.GetFromArray(row, col + 1, unknownUniverse) +
-                outputImage.GetFromArray(row + 1, col - 1, unknownUniverse) +
-                outputImage.GetFromArray(row + 1, col, unknownUniverse) +
-                outputImage.GetFromArray(row + 1, col + 1, unknownUniverse);
-
-            pixelString = pixelString
-                .Replace(".", "0")
-                .Replace("#", "1");
+                (outputImage.GetFromArray(row - 1, col - 1, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row - 1, col, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row - 1, col + 1, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row, col - 1, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row, col, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row, col + 1, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row + 1, col - 1, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row + 1, col, unknownUniverse) == "#" ? "1" : "0") +
+                (outputImage.GetFromArray(row + 1, col + 1, unknownUniverse) == "#" ? "1" : "0");
             
-            var binaryResult = Convert.ToInt32(pixelString, 2);
-
-            return Algorhithm.Substring(binaryResult, 1);
+            return Algorhithm.Substring(Convert.ToInt32(pixelString, 2), 1);
         }
 
         private void PrintImage(string[,] image)
