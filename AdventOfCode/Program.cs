0a6ad71457456bc6bc20dffc9f576c5c87521071
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2021-12-20 17:05:30 +0100

    Small day 19 improvements

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,8 +6,8 @@ namespace AdventOfCode
     class Program
     {
         public static int Year = 2021;
-        public static int SkipDays = 19;
-        public static int CurrentDay = 20;
+        public static int SkipDays = 18;
+        public static int CurrentDay = 19;
         
         static void Main(string[] args)
         {
